1383b4c06b2979ee6400e14013a8ddf0526411a3
Nadine Wischmeier <nadine.wischmeier@outlook.com>
2022-01-24 15:33:18 +0100

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Heading() {
+    return (
+    <header > 
+    <h1 className="">Keeper</h1>
+     </header>
+)
+}
+
+
+export default Heading;
---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+function Note() {
+    return (<div className="note"> 
+    <h1>Lorem ipsum</h1>
+    <p>blasi blusi blasi blusi</p>
+    </div>)
+}
+
+export default Note; 
