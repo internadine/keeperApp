54127db9703f1ee666831d396c282042fbdc65a7
internadine <nadine.wischmeier@outlook.com>
2022-02-08 21:26:54 +0100

    import notes from js-file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,18 @@ import React from "react"
 import Heading from "./Header"
 import Footer from "./Footer"
 import Note from "./Note"
+import notes from "../notes"
 
 function App() {
-    return ( <div>
-    <Heading />
-    <Note />
-    <Footer />
+    return (<div>
+        <Heading />
+        {notes.map((note) => {
+            return <Note title={note.title} content={note.content} />
+        })}
+
+        <Footer />
     </div>)
-   
+
 }
 
 export default App;
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,10 @@
 import React from "react"
 
-function Note() {
-    return (<div className="note"> 
-    <h1>Lorem ipsum</h1>
-    <p>blasi blusi blasi blusi</p>
+function Note(props) {
+    return (<div className="note">
+        <h1>{props.title}</h1>
+        <p>{props.content}</p>
     </div>)
 }
 
-export default Note; 
+export default Note;
